38dd02b317e0ca978f47274900a0f5e35f8ce223
NarhiAkseli <narhiaks@gmail.com>
2024-02-22 13:11:17 +0200

    Read n write database working

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from app import db
 
 class Vehicle(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'Vehicle'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
+
