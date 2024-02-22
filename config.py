294223394ff57d26f3f73e848ff4943980c8435e
NarhiAkseli <98940469+NarhiAkseli@users.noreply.github.com>
2024-02-22 14:21:42 +0200

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:aDbGfCCd533Da-CHcfgdGf3-Cg1hH1FF@roundhouse.proxy.rlwy.net:49193/railway'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
