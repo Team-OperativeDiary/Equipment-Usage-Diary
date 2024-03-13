c5cbbc49259241f6a715aca45e04fab7c49a7010
NarhiAkseli <narhiaks@gmail.com>
2024-03-13 10:54:12 +0200

    update routes and html

--- app/routes.py
+++ app/routes.py
@@ -9,46 +9,41 @@ def main_page():
     return render_template('main_page.html', machines=machines)
 
 machines = [
-    {"name": "JCB JS 210 LC -11", "url": "/jcb-js-210-lc-11"},
-    {"name": "JCB JS 210 LC -18", "url": "/jcb-js-210-lc-18"},
-    {"name": "Liebherr R 920 -14", "url": "/liebherr-r-920-14"},
-    {"name": "New Holland 135 SR -08", "url": "/new-holland-135-sr-08"},
-    {"name": "New Holland 135 BSR-2 -10", "url": "/new-holland-135-bsr-2-10"},
-    {"name": "Pyöräalustaiset", "url": "/pyoraalustaiset"},
-    {"name": "Caterpillar M 313 -15", "url": "/caterpillar-m-313-15"},
-    {"name": "Caterpillar M315 -99", "url": "/caterpillar-m315-99"},
-    {"name": "JCB JS 160 W -12", "url": "/jcb-js-160-w-12"},
-    {"name": "Wacker Neuson EW 100 -15", "url": "/wacker-neuson-ew-100-15"},
-    {"name": "Mini/midikaivukoneet", "url": "/mini-midikaivukoneet"},
-    {"name": "Cat 308E2 -17", "url": "/cat-308e2-17"},
-    {"name": "ECM ES 85 SB4 -18", "url": "/ecm-es-85-sb4-18"},
-    {"name": "Volvo EC 35 -02", "url": "/volvo-ec-35-02"},
-    {"name": "Sunward 80 -13", "url": "/sunward-80-13"},
-    {"name": "Liikennetraktorit", "url": "/liikennetraktorit"},
-    {"name": "Volvo DR 25 A -97", "url": "/volvo-dr-25-a-97"},
-    {"name": "Volvo DR 5350 -84", "url": "/volvo-dr-5350-84"},
-    {"name": "Pyöräkuormaajat ym. kuormauskoneet", "url": "/pyorakuormaajat-ym-kuormauskoneet"},
-    {"name": "Atlast AR 60 -06", "url": "/atlast-ar-60-06"},
-    {"name": "Hyundai 740 HL TM3 -02", "url": "/hyundai-740-hl-tm3-02"},
-    {"name": "Volvo 110 F -08", "url": "/volvo-110-f-08"},
-    {"name": "Volvo L30 D-Z -01", "url": "/volvo-l30-d-z-01"},
-    {"name": "JCB Robot -03", "url": "/jcb-robot-03"},
-    {"name": "JCB 421", "url": "/jcb-421"},
-    {"name": "Kauvurikuormaajat", "url": "/kauvurikuormaajat"},
-    {"name": "Lännen 860S -99", "url": "/lannen-860s-99"},
-    {"name": "Muut maarakkenuskoneet", "url": "/muut-maarakkenuskoneet"},
-    {"name": "Caterpillar D 4 Puskutraktori", "url": "/caterpillar-d-4-puskutraktori"},
-    {"name": "New Holland TL 100A Traktori", "url": "/new-holland-tl-100a-traktori"},
-    {"name": "John Deere 6534 Traktori", "url": "/john-deere-6534-traktori"},
-    {"name": "Lännen AH 162 Tiehöylä, runko-ohjaus", "url": "/lannen-ah-162-tiehoyla-runko-ohjaus"},
-    {"name": "Autot", "url": "/autot"},
-    {"name": "Scania G142 Tienhoitovarustus", "url": "/scania-g142-tienhoitovarustus"},
+    {"name": "JCB JS 210 LC -11", "url": "/jcb-js-210-lc-11", "category": "Tela-alustaiset"},
+    {"name": "JCB JS 210 LC -18", "url": "/jcb-js-210-lc-18", "category": "Tela-alustaiset"},
+    {"name": "Liebherr R 920 -14", "url": "/liebherr-r-920-14", "category": "Tela-alustaiset"},
+    {"name": "New Holland 135 SR -08", "url": "/new-holland-135-sr-08", "category": "Tela-alustaiset"},
+    {"name": "New Holland 135 BSR-2 -10", "url": "/new-holland-135-bsr-2-10", "category": "Tela-alustaiset"},
+    {"name": "Caterpillar M 313 -15", "url": "/caterpillar-m-313-15", "category": "Pyöräalustaiset"},
+    {"name": "Caterpillar M315 -99", "url": "/caterpillar-m315-99", "category": "Pyöräalustaiset"},
+    {"name": "JCB JS 160 W -12", "url": "/jcb-js-160-w-12", "category": "Pyöräalustaiset"},
+    {"name": "Wacker Neuson EW 100 -15", "url": "/wacker-neuson-ew-100-15", "category": "Pyöräalustaiset"},
+    {"name": "Cat 308E2 -17", "url": "/cat-308e2-17", "category": "Mini/midikaivukoneet"},
+    {"name": "ECM ES 85 SB4 -18", "url": "/ecm-es-85-sb4-18", "category": "Mini/midikaivukoneet"},
+    {"name": "Volvo EC 35 -02", "url": "/volvo-ec-35-02", "category": "Mini/midikaivukoneet"},
+    {"name": "Sunward 80 -13", "url": "/sunward-80-13", "category": "Mini/midikaivukoneet"},
+    {"name": "Volvo DR 25 A -97", "url": "/volvo-dr-25-a-97", "category": "Liikennetraktorit"},
+    {"name": "Volvo DR 5350 -84", "url": "/volvo-dr-5350-84", "category": "Liikennetraktorit"},
+    {"name": "Atlast AR 60 -06", "url": "/atlast-ar-60-06", "category": "kuormauskoneet"},
+    {"name": "Hyundai 740 HL TM3 -02", "url": "/hyundai-740-hl-tm3-02", "category": "kuormauskoneet"},
+    {"name": "Volvo 110 F -08", "url": "/volvo-110-f-08", "category": "kuormauskoneet"},
+    {"name": "Volvo L30 D-Z -01", "url": "/volvo-l30-d-z-01", "category": "kuormauskoneet"},
+    {"name": "JCB Robot -03", "url": "/jcb-robot-03", "category": "kuormauskoneet"},
+    {"name": "JCB 421", "url": "/jcb-421", "category": "kuormauskoneet"},
+    {"name": "Lännen 860S -99", "url": "/lannen-860s-99", "category": "Kauvurikuormaajat"},
+    {"name": "Caterpillar D 4 Puskutraktori", "url": "/caterpillar-d-4-puskutraktori", "category": "Muut maarakkenuskoneet"},
+    {"name": "New Holland TL 100A Traktori", "url": "/new-holland-tl-100a-traktori", "category": "Muut maarakkenuskoneet"},
+    {"name": "John Deere 6534 Traktori", "url": "/john-deere-6534-traktori", "category": "Muut maarakkenuskoneet"},
+    {"name": "Lännen AH 162 Tiehöylä, runko-ohjaus", "url": "/lannen-ah-162-tiehoyla-runko-ohjaus", "category": "Muut maarakkenuskoneet"},
+    {"name": "Scania G142 Tienhoitovarustus", "url": "/scania-g142-tienhoitovarustus", "category": "Autot"},
 ]
 
-@main_bp.route('/<string:machine_name>')
-def machine_details(machine_name):
-    # Your logic to retrieve machine details based on the machine_name
-    return f"Details for machine: {machine_name}"
+@main_bp.route('/<category>/<machine_name>')
+def machine_details(category, machine_name):
+    # Your logic to retrieve machine details based on the category and machine_name
+    # For now, let's just return a string with the category and machine name
+    return f"Details for machine: {machine_name} in category: {category}"
+
 
 @main_bp.route('/form', methods=['GET', 'POST'])
 def form():
