4c059e724239b032d4689f4736edfa42b0da372b
NarhiAkseli <narhiaks@gmail.com>
2024-04-23 11:09:59 +0300

    update lol

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,9 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from app import db
-from app.models import MaintenanceItem
-from app.models import Vehicle
-from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, IntegerField, DateField
-from wtforms.validators import DataRequired, NumberRange, Length
-
+from app.models import MaintenanceItem, Vehicle
+from sqlalchemy.orm.exc import NoResultFound
 
 main_bp = Blueprint('main', __name__)
-
-@main_bp.route('/')
-def main_page():
-    return render_template('main_page.html', machines=machines)
-
 
 machines = [
     {"name": "JCB JS 210 LC -11", "url": "/jcb-js-210-lc-11", "category": "Tela-alustaiset"},
@@ -44,90 +35,74 @@ machines = [
     {"name": "Scania G142 Tienhoitovarustus", "url": "/scania-g142-tienhoitovarustus", "category": "Autot"},
 ]
 
-
-class MaintenanceForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=50)])
-    date = DateField('Date', validators=[DataRequired()])
-    driving_hours = IntegerField('Driving Hours', validators=[DataRequired(), NumberRange(min=0)])
-    oil_checked = BooleanField('Oil Checked')
-    oil_added_amount = IntegerField('Oil Added Amount', validators=[NumberRange(min=0)])
-    hydraulic_oil_checked = BooleanField('Hydraulic Oil Checked')
-    hydraulic_oil_added_amount = IntegerField('Hydraulic Oil Added Amount', validators=[NumberRange(min=0)])
-    gear_oil_checked = BooleanField('Gear Oil Checked')
-    gear_oil_added_amount = IntegerField('Gear Oil Added Amount', validators=[NumberRange(min=0)])
-    liquid_coolant_checked = BooleanField('Liquid Coolant Checked')
-    liquid_coolant_added_amount = IntegerField('Liquid Coolant Added Amount', validators=[NumberRange(min=0)])
-    fuel_added = IntegerField('Fuel Added', validators=[NumberRange(min=0)])
-    greasing_checked = BooleanField('Greasing Checked')
-    automatic_greaser_checked = BooleanField('Automatic Greaser Checked')
-    automatic_greaser_last_date_filled = DateField('Automatic Greaser Last Date Filled')
+@main_bp.route('/')
+def main_page():
+    return render_template('main_page.html', machines=machines)
 
 @main_bp.route('/<category>/<machine_name>', methods=['GET', 'POST'])
 def machine_details(category, machine_name):
     vehicle: Vehicle = db.session.query(Vehicle).filter_by(name=find_matching_vehicle_by_url("/" + machine_name)).first()
-    form = MaintenanceForm()
             
     if request.method == 'POST':
-        if form.validate_on_submit():
-            # If the request method is POST and the form is valid, handle form submission
-            # Get form data
-            vehicle_id =  vehicle.vehicle_id
-            username = form.username.data
-            date = form.date.data
-            driving_hours = form.driving_hours.data
-            oil_checked = form.oil_checked.data
-            oil_added_amount = form.oil_added_amount.data
-            hydraulic_oil_checked = form.hydraulic_oil_checked.data
-            hydraulic_oil_added_amount = form.hydraulic_oil_added_amount.data
-            gear_oil_checked = form.gear_oil_checked.data
-            gear_oil_added_amount = form.gear_oil_added_amount.data
-            liquid_coolant_checked = form.liquid_coolant_checked.data
-            liquid_coolant_added_amount = form.liquid_coolant_added_amount.data
-            fuel_added = form.fuel_added.data
-            greasing_checked = form.greasing_checked.data
-            automatic_greaser_checked = form.automatic_greaser_checked.data
-            automatic_greaser_last_date_filled = form.automatic_greaser_last_date_filled.data
+        # If the request method is POST, handle form submission
+        # Get form data
+        vehicle_id =  vehicle.vehicle_id
+        username = request.form['username']
+        date = request.form['date']
+        driving_hours = request.form['drivingHours']
+        oil_checked = 'oilChecked' in request.form
+        oil_added_amount = request.form['oilAddedAmount'] if 'oilAddedAmount' in request.form else None
+        hydraulic_oil_checked = 'hydraulicOilChecked' in request.form
+        hydraulic_oil_added_amount = request.form['hydraulicOilAddedAmount'] if 'hydraulicOilAddedAmount' in request.form else None
+        gear_oil_checked = 'gearOilChecked' in request.form
+        gear_oil_added_amount = request.form['gearOilAddedAmount'] if 'gearOilAddedAmount' in request.form else None
+        liquid_coolant_checked = 'liquidCoolantChecked' in request.form
+        liquid_coolant_added_amount = request.form['liquidCoolantAddedAmount'] if 'liquidCoolantAddedAmount' in request.form else None
+        fuel_added = request.form['fuelAdded'] if 'fuelAdded' in request.form else None
+        greasing_checked = 'greasingChecked' in request.form
+        automatic_greaser_checked = 'automaticGreaserChecked' in request.form
+        automatic_greaser_last_date_filled = request.form['automaticGreaserLastDateFilled'] if 'automaticGreaserLastDateFilled' in request.form else None
 
-            # Create a MaintenanceItem object and add it to the database
-            maintenance_item = MaintenanceItem( 
-                vehicle_id=vehicle_id, 
-                username=username,
-                date=date,
-                driving_hours=driving_hours,
-                oil_checked=oil_checked,
-                oil_added_amount=oil_added_amount,
-                hydraulic_oil_checked=hydraulic_oil_checked,
-                hydraulic_oil_added_amount=hydraulic_oil_added_amount,
-                gear_oil_checked=gear_oil_checked,
-                gear_oil_added_amount=gear_oil_added_amount,
-                liquid_coolant_checked=liquid_coolant_checked,
-                liquid_coolant_added_amount=liquid_coolant_added_amount,
-                fuel_added=fuel_added,
-                greasing_checked=greasing_checked,
-                automatic_greaser_checked=automatic_greaser_checked,
-                automatic_greaser_last_date_filled=automatic_greaser_last_date_filled
-            )
-            db.session.add(maintenance_item)
-            db.session.commit()
-            # If the request method is POST and the form is valid, redirect to the results page
-            return redirect(url_for('main.results'))
+        # Create a MaintenanceItem object and add it to the database
+        maintenance_item = MaintenanceItem( 
+            vehicle_id=vehicle_id, 
+            username=username,
+            date=date,
+            driving_hours=driving_hours,
+            oil_checked=oil_checked,
+            oil_added_amount=oil_added_amount,
+            hydraulic_oil_checked=hydraulic_oil_checked,
+            hydraulic_oil_added_amount=hydraulic_oil_added_amount,
+            gear_oil_checked=gear_oil_checked,
+            gear_oil_added_amount=gear_oil_added_amount,
+            liquid_coolant_checked=liquid_coolant_checked,
+            liquid_coolant_added_amount=liquid_coolant_added_amount,
+            fuel_added=fuel_added,
+            greasing_checked=greasing_checked,
+            automatic_greaser_checked=automatic_greaser_checked,
+            automatic_greaser_last_date_filled=automatic_greaser_last_date_filled
+        )
+        db.session.add(maintenance_item)
+        db.session.commit()
+        # If the request method is POST, redirect to the results page
+        return redirect(url_for('main.results'))
     else:
         # If the request method is GET, render the 'form.html' template with the provided category and machine_name
-        return render_template('form.html', category=category, machine_name=machine_name, form=form)
-
+        return render_template('form.html', category=category, machine_name=machine_name)
 
 @main_bp.route('/results')
 def results():
     maintenance_items = MaintenanceItem.query.all()
-
+    
+    # Print attributes of each maintenance item to the command line
+    for item in maintenance_items:
+        print(f"Username: {item.username}, Date: {item.date}, Driving Hours: {item.driving_hours}")
+        # Add more attributes as needed
+        
     return render_template('results.html', maintenance_items=maintenance_items)
-
-
-
 
 @main_bp.route('/delete/<int:item_id>', methods=['POST'])
 def delete_item(item_id):
-
     item = MaintenanceItem.query.get_or_404(item_id)
     
     db.session.delete(item)
