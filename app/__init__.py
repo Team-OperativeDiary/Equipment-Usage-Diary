d2fd3b57c8487458a9b9d2b5f537cb390013ea06
NarhiAkseli <narhiaks@gmail.com>
2024-05-24 12:45:31 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,22 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
-app.config.from_object(Config)
+app.config.from_mapping(
+    SECRET_KEY=os.getenv('SECRET_KEY'),
+    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
+)
+
 
 db = SQLAlchemy(app)
 
 from app.routes import main_bp
 app.register_blueprint(main_bp)
+
+
