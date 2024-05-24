81eb75984cb8067fdb9b79c62a197c94e3f07e85
NarhiAkseli <narhiaks@gmail.com>
2024-05-24 13:22:14 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
+from config import Config
 
 app = Flask(__name__)
-app.config.from_mapping(
-    SECRET_KEY=os.getenv('SECRET_KEY'),
-    SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL'),
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
-)
-
+app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 
