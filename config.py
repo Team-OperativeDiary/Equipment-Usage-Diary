03f45e8e014a38bcf2427b932ecd1770cf60ac85
NarhiAkseli <narhiaks@gmail.com>
2024-05-24 14:14:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
-    #connection string poistettu
+    #connection string poistettu jippii
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
