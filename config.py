ef73489236a0109c0c86958e8aef30d36f141c60
NarhiAkseli <narhiaks@gmail.com>
2024-05-24 13:19:21 +0300

    revert

---
+++ config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:TftCgvCwteroOfTgOhAXdKZILhXjbGuy@roundhouse.proxy.rlwy.net:49475/railway'  #local database connection string
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
