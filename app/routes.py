4e05cdf2982ecbe8de23aca819e8b38a132b3be9
NarhiAkseli <narhiaks@gmail.com>
2024-05-24 10:22:57 +0300

    to do: fix routes

--- app/routes.py
+++ app/routes.py
@@ -6,35 +6,6 @@ from sqlalchemy.orm.exc import NoResultFound
 
 main_bp = Blueprint('main', __name__)
 
-machines = [
-    {"name": "JCB JS 210 LC -11", "url": "/jcb-js-210-lc-11", "category": "Tela-alustaiset"},
-    {"name": "JCB JS 210 LC -18", "url": "/jcb-js-210-lc-18", "category": "Tela-alustaiset"},
-    {"name": "Liebherr R 920 -14", "url": "/liebherr-r-920-14", "category": "Tela-alustaiset"},
-    {"name": "New Holland 135 SR -08", "url": "/new-holland-135-sr-08", "category": "Tela-alustaiset"},
-    {"name": "New Holland 135 BSR-2 -10", "url": "/new-holland-135-bsr-2-10", "category": "Tela-alustaiset"},
-    {"name": "Caterpillar M 313 -15", "url": "/caterpillar-m-313-15", "category": "Pyöräalustaiset"},
-    {"name": "Caterpillar M315 -99", "url": "/caterpillar-m315-99", "category": "Pyöräalustaiset"},
-    {"name": "JCB JS 160 W -12", "url": "/jcb-js-160-w-12", "category": "Pyöräalustaiset"},
-    {"name": "Wacker Neuson EW 100 -15", "url": "/wacker-neuson-ew-100-15", "category": "Pyöräalustaiset"},
-    {"name": "Cat 308E2 -17", "url": "/cat-308e2-17", "category": "Mini/midikaivukoneet"},
-    {"name": "ECM ES 85 SB4 -18", "url": "/ecm-es-85-sb4-18", "category": "Mini/midikaivukoneet"},
-    {"name": "Volvo EC 35 -02", "url": "/volvo-ec-35-02", "category": "Mini/midikaivukoneet"},
-    {"name": "Sunward 80 -13", "url": "/sunward-80-13", "category": "Mini/midikaivukoneet"},
-    {"name": "Volvo DR 25 A -97", "url": "/volvo-dr-25-a-97", "category": "Liikennetraktorit"},
-    {"name": "Volvo DR 5350 -84", "url": "/volvo-dr-5350-84", "category": "Liikennetraktorit"},
-    {"name": "Atlast AR 60 -06", "url": "/atlast-ar-60-06", "category": "kuormauskoneet"},
-    {"name": "Hyundai 740 HL TM3 -02", "url": "/hyundai-740-hl-tm3-02", "category": "kuormauskoneet"},
-    {"name": "Volvo 110 F -08", "url": "/volvo-110-f-08", "category": "kuormauskoneet"},
-    {"name": "Volvo L30 D-Z -01", "url": "/volvo-l30-d-z-01", "category": "kuormauskoneet"},
-    {"name": "JCB Robot -03", "url": "/jcb-robot-03", "category": "kuormauskoneet"},
-    {"name": "JCB 421", "url": "/jcb-421", "category": "kuormauskoneet"},
-    {"name": "Lännen 860S -99", "url": "/lannen-860s-99", "category": "Kaivurikuormaajat"},
-    {"name": "Caterpillar D 4 Puskutraktori", "url": "/caterpillar-d-4-puskutraktori", "category": "Muut maarakkenuskoneet"},
-    {"name": "New Holland TL 100A Traktori", "url": "/new-holland-tl-100a-traktori", "category": "Muut maarakkenuskoneet"},
-    {"name": "John Deere 6534 Traktori", "url": "/john-deere-6534-traktori", "category": "Muut maarakkenuskoneet"},
-    {"name": "Lännen AH 162 Tiehöylä, runko-ohjaus", "url": "/lannen-ah-162-tiehoyla-runko-ohjaus", "category": "Muut maarakkenuskoneet"},
-    {"name": "Scania G142 Tienhoitovarustus", "url": "/scania-g142-tienhoitovarustus", "category": "Autot"},
-]
 
 @main_bp.route('/')
 def main_page():
@@ -51,33 +22,16 @@ def main_page():
     
     return render_template('main_page.html', categorized_items=categorized_items)
 
-def extract_name_and_model_from_url(url):
-    # Remove leading and trailing slashes, then split at the first "-" occurrence
-    parts = url.strip("/").split("-", 1)
-    if len(parts) == 2:
-        # Extract the name and model, replacing "-" with space
-        name = parts[0]
-        model = parts[1].replace("-", " ")
-        return name, model
-    else:
-        return None, None
-
 
     
 @main_bp.route('/<category>/<machine_name>', methods=['GET', 'POST'])
 def machine_details(category, machine_name):
-    name, model = extract_name_and_model_from_url(machine_name)
-
-    vehicle: Vehicle = db.session.query(Vehicle).filter_by(name=find_matching_vehicle_by_url("/" + machine_name)).first()
-    vehicle = db.session.query(Vehicle).filter_by().first()
-    print(f"Name: {name}, Model: {model}")
-
+       
    
             
     if request.method == 'POST':
         # If the request method is POST, handle form submission
         # Get form data
-        vehicle_id = vehicle.id
         username = request.form['username']
         date = request.form['date']
         driving_hours = request.form['drivingHours']
@@ -143,8 +97,3 @@ def delete_item(item_id):
     
     return redirect(url_for('main.results'))
 
-def find_matching_vehicle_by_url(url):
-    for vehicle in machines:
-        if vehicle['url'] == url:
-            return vehicle['name']
-    return None
